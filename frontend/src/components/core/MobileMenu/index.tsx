cb111f08259ea774e55d0e97746b7d2f2ba2f161
Saulo Felipe <77216487+Saulo-Felipe@users.noreply.github.com>
2024-01-10 10:36:01 -0300

    Update index.tsx

--- frontend/src/components/core/MobileMenu/index.tsx
+++ frontend/src/components/core/MobileMenu/index.tsx
@@ -68,7 +68,7 @@ export function MobileMenu({ setMenuMobileIsOpen, menuMobileIsOpen }: MobileMenu
           <h2><GiTank /> Multiplayer Games</h2>
           <hr />
 
-          <a rel="noopener noreferrer" href={"https://multiplayer-game-saulo.herokuapp.com"} target={"_blank"}>
+          <a rel="noopener noreferrer" href={"https://tanksfight-server.saulofelipe.tech"} target={"_blank"}>
             <div className={styles.game}>
               <div className={styles.gameTitle}>Jogue com seus amigos :D</div>
             </div>
